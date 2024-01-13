9978f435fcf187eb42250f331e8f6bf733824efc
Admin <Admin@LAPTOP-2HDCC6PU>
2024-01-13 15:16:09 +0300

    Добавьте файлы проекта.

---
+++ DataPeople_RVP_Yurl/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace DataPeople_RVP_Yurl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonFilling = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.buttonStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonFilling
+            // 
+            this.buttonFilling.Location = new System.Drawing.Point(569, 56);
+            this.buttonFilling.Name = "buttonFilling";
+            this.buttonFilling.Size = new System.Drawing.Size(193, 32);
+            this.buttonFilling.TabIndex = 1;
+            this.buttonFilling.Text = "Начать заполнение";
+            this.buttonFilling.UseVisualStyleBackColor = true;
+            this.buttonFilling.Click += new System.EventHandler(this.buttonFilling_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(609, 312);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(116, 40);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "Сохранить";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(609, 376);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(116, 34);
+            this.buttonLoad.TabIndex = 3;
+            this.buttonLoad.Text = "Загрузить";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 62;
+            this.dataGridView.RowTemplate.Height = 28;
+            this.dataGridView.Size = new System.Drawing.Size(551, 398);
+            this.dataGridView.TabIndex = 4;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 500;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(609, 148);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(127, 39);
+            this.buttonStop.TabIndex = 5;
+            this.buttonStop.Text = "Стоп";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonFilling);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonFilling;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button buttonStop;
+    }
+}
+
---
+++ DataPeople_RVP_Yurl/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataPeople_RVP_Yurl
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            InitDataGridView();
+        }
+
+        private void InitDataGridView()
+        {
+
+            // Создаем столбцы
+            dataGridView.Columns.Add("Name", "Имя");
+            dataGridView.Columns.Add("Height", "Рост");
+            dataGridView.Columns.Add("Weight", "Вес");
+
+            // Создаем столбец для группы крови
+            var bloodGroupColumn = new DataGridViewComboBoxColumn
+            {
+                HeaderText = "Группа крови",
+                Name = "BloodGroup",
+                DataSource = new string[] { "A", "B", "AB" }
+            };
+
+            dataGridView.Columns.Add(bloodGroupColumn);
+
+            
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Add(getRandomName(), random.Next(140, 200), random.Next(50, 120), getRandomBloodGroup());
+        }
+        private string getRandomName()
+        {
+            string[] names = { "Alice", "Bob", "Charlie", "David", "Eve" };
+            return names[random.Next(names.Length)];
+        }
+
+        private string getRandomBloodGroup()
+        {
+            string[] bloodGroups = { "A", "B", "AB" };
+            return bloodGroups[random.Next(bloodGroups.Length)];
+        }
+
+        private void buttonFilling_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            using (var sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "CSV files (*.csv)|*.csv";
+                sfd.Title = "Save as CSV File";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    using (var sw = new StreamWriter(sfd.FileName))
+                    {
+                        // Записываем заголовки столбцов
+                        foreach (DataGridViewColumn column in dataGridView.Columns)
+                        {
+                            sw.Write(column.HeaderText + ";");
+                        }
+                        sw.WriteLine();
+
+                        // Записываем данные из DataGridView
+                        foreach (DataGridViewRow row in dataGridView.Rows)
+                        {
+                            foreach (DataGridViewCell cell in row.Cells)
+                            {
+                                sw.Write(cell.Value + ";");
+                            }
+                            sw.WriteLine();
+                        }
+                    }
+                }
+            }
+            MessageBox.Show("Файл успешно сохранён!");
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "CSV files (*.csv)|*.csv";
+                ofd.Title = "Load from CSV File";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    dataGridView.Rows.Clear(); // Очистка существующих данных в DataGridView
+
+                    using (var sr = new StreamReader(ofd.FileName))
+                    {
+                        string[] headers = sr.ReadLine().Split(';'); // Прочитываем заголовки столбцов
+
+                        while (!sr.EndOfStream)
+                        {
+                            string[] rows = sr.ReadLine().Split(';');
+                            dataGridView.Rows.Add(rows);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
